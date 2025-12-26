668b506298cd7d5d475ce7e6e371fe62079cdef9
Your Name <youremail@example.com>
2025-12-27 00:36:22 +0530

    CORS

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:5173",
-  "https://saabz.netlify.app",
+  "https://saabz.onrender.com/",
 ];
 
 app.use(
--- src/components/layout/PublicLayout.tsx
+++ src/components/layout/PublicLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "@/components/layout/Navbar";
-import { Footer } from "@/components/layout/Footer";
 
 export default function PublicLayout() {
   return (
