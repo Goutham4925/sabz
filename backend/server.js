b270723d432338cf137b89a4475f482982a3d56f
Your Name <youremail@example.com>
2025-12-27 00:41:55 +0530

    CORS

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:5173",
-  "https://saabz.onrender.com/",
+  "https://saabz.onrender.com",
 ];
 
 app.use(
