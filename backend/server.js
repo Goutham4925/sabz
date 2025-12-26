bcb00e002b3b637323f9594fc93fc7dbd9c8a6be
Your Name <youremail@example.com>
2025-12-26 16:29:51 +0530

    diverted db

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:5173",
-  "https://sabz-kitchen.onrender.com", 
+  "https://saabz-kitchen.onrender.com", 
 ];
 
 app.use(
