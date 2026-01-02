b6f6dc7e180e5b6ea816d9bdcb51ac7de3f3a1c7
Your Name <youremail@example.com>
2026-01-02 19:04:06 +0530

    updated final 1

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,9 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:5173",
-  "https://saabz.onrender.com",
   "https://sabz-sage.vercel.app",
+  "https://www.saabz.in",
+
 ];
 
 app.use(
