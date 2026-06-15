3dee9b26223cffbc3bd0b3764ee84452b6c8ad66
Goutham4925 <gouthamgo49251@gmail.com>
2026-06-15 19:29:40 +0530

    version 2 client update v3

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,10 +1,7 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  family: 4,
+  service: "gmail",
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
